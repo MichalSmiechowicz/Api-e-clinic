07cb459c5c0642778c2e0a2b9676557fb54ae120
Kuba <jakubszadkowski210495@wp.pl>
2020-09-20 22:39:52 +0200

    Changing pesel and avatar path in sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,20 +3,20 @@
 -- dummy data
 -- Doc
 
-insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('kowalski.jpg', '1984-02-13', 'descr', 'mail5', 'Krzysztof', 'pass', 'pesel','654987123','Internista', 'Kowalski', 'doctor');
-insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('nowak.jpg', '1985-05-01', 'descr', 'mail6', 'Adam', 'pass', 'pesel','123456789','Internista', 'Nowak', 'doctor');
-insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('michalik.jpg', '1970-11-23', 'descr', 'mail7', 'Jadwiga', 'pass', 'pesel','645879213','Neurolog', 'Michalik', 'doctor');
+insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('kowalski.jpg', '1984-02-13', 'descr', 'mail5', 'Krzysztof', '84021351671', 'pesel','654987123','Internista', 'Kowalski', 'doctor');
+insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('nowak.jpg', '1985-05-01', 'descr', 'mail6', 'Adam', 'pass', '85050182739','123456789','Internista', 'Nowak', 'doctor');
+insert into doctors (AVATAR_PATH, BIRTH_DATE  ,DESCRIPTION ,EMAIL ,NAME ,PASSWORD ,PESEL ,PHONE ,SPECIALIZATION ,SURNAME, role ) values('michalik.jpg', '1970-11-23', 'descr', 'mail7', 'Jadwiga', 'pass', '70112364268','645879213','Neurolog', 'Michalik', 'doctor');
 
 -- User
 
-insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Sienkiewicza 63/14 Łódź 94-212', 'avatar path','2001-01-11', 'Alergia', 'mail1', 'm', 'Arkadiusz', 'pass', 'pesel', '600125974', 'Sikorski', 'user');
-insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Łódzka 12 Tuszyn 95-080', 'czerwinska.jpg','1998-04-07', 'brak', 'mail2', 'k', 'Aleksandra', 'pass', 'pesel', '798459781', 'Czerwińska', 'user');
-insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Wschodnia 24/10 Zgierz 92-021', 'avatar path','2001-08-30', ' Astma', 'mail3', 'm', 'Marcel', 'pass', 'pesel', '500149988', 'Wróblewski', 'user');
-insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Bandurskiego 33/14 Łódź 94-045', 'avatar path','1994-12-30', ' Padaczka', 'mail4', 'k', 'Lidia', 'pass', 'pesel', '658978145', 'Kucharska', 'user');
+insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Sienkiewicza 63/14 Łódź 94-212', 'sikorski.jpg path','2001-01-11', 'Alergia', 'mail1', 'm', 'Arkadiusz', 'pass', '01211178474', '600125974', 'Sikorski', 'user');
+insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Łódzka 12 Tuszyn 95-080', 'czerwinska.jpg','1998-04-07', 'brak', 'mail2', 'k', 'Aleksandra', 'pass', '98040711349', '798459781', 'Czerwińska', 'user');
+insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Wschodnia 24/10 Zgierz 92-021', 'wroblewski.jpg','2001-08-30', ' Astma', 'mail3', 'm', 'Marcel', 'pass', '01283082653', '500149988', 'Wróblewski', 'user');
+insert into users (address, avatar_path, birth_date, chronic_diseases, email, gender, name, password,pesel, phone, surname, role) values('Bandurskiego 33/14 Łódź 94-045', 'kucharska.jpg','1994-12-30', ' Padaczka', 'mail4', 'k', 'Lidia', 'pass', '94123039641', '658978145', 'Kucharska', 'user');
 
 -- Reception
 
-insert into reception (name, surname, phone, email, password, avatar_Path, role) values ('Janusz' , 'Xd' , '123789456', 'mail10', 'pass','xd.jpg','reception');
+insert into reception (name, surname, phone, email, password, avatar_Path, role) values ('Janusz' , 'Januszewski' , '123789456', 'mail10', 'pass','januszewski.jpg','reception');
 
 -- Appointments
 
